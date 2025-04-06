dff23c2e381dcacc94d81b96ddb49296a79f4bd2
satyam-pokee <satyam-pokee@gmail.com>
2025-04-06 23:41:35 +0530

    design changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { EditorComponent } from './editor/editor.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-     { path: '', component: BlogListComponent },
+  { path: '', component: HomeComponent },
+     { path: 'blog', component: BlogListComponent },
   { path: 'create', component: EditorComponent }
 ];
